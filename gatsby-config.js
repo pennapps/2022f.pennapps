57fd63fbacf00f841f6272e8aaec86cebe35ceaf
Yuying Fan <yuyingfanus@gmail.com>
2022-06-08 23:59:48 -0400

    added render section contents from mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,21 @@ module.exports = {
     title: `2022f.pennapps`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: ["gatsby-plugin-mdx",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     },
-    __key: "pages"
-  }]
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/content/`,
+      },
+    },
+  ]
 };
