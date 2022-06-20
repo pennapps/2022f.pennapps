760eda039d23f99715fd25e9478ddf4c76c1a213
Jeffrey Qu <40805785+jqu-16@users.noreply.github.com>
2022-06-20 12:47:00 -0700

    Properly render markdown for answers

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,16 +1,26 @@
 import * as React from 'react'
 import Collapsible from 'react-collapsible';
+import ReactMarkdown from 'react-markdown'
 import '../styles/index.css'
 
 const FAQ = ({content, key}) => {
+
+  function parse(answer) {
+    return answer.replace(/_*_/, 'i')
+  }
+
+
   let faqs = content.split("## ").slice(1);
   let output = faqs.map((faq) => {
-    let tokens = faq.split("?\r\n\r\n");
+    let tokens = faq.split("\r\n\r\n");
     console.log(tokens);
     let question = tokens[0];
-    let answer = tokens[1];
-    return <Collapsible trigger={<h2>{`${question}?`}</h2>}>
-      <p>{answer}</p>
+    let answers = [];
+    for (let i = 1; i < tokens.length; i++) {
+      answers.push(<p><ReactMarkdown>{tokens[i]}</ReactMarkdown></p>);
+    }
+    return <Collapsible trigger={<h2>{`${question}`}</h2>}>
+      {answers}
     </Collapsible>;
   });
 
