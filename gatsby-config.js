619fed031880cb2d1b25e6966bd6ccbf7e78c198
audrey-yang <ayang24.robin@gmail.com>
2022-06-23 21:39:48 -0700

    Style and content updates Fix anchor issue, add favicon, tbd logistics

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
   siteMetadata: {
     title: `2022f.pennapps`,
     siteUrl: `https://www.pennapps.github.io/2022f.pennapps/`,
-    favicon: 'src/images/favicon.png'
   },
   plugins: ["gatsby-plugin-mdx",
     {
