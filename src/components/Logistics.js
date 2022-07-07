028676290d38da453326f692777877bd583a0f8e
vcai122 <vincent.delaware@gmail.com>
2022-07-07 15:30:45 -0400

    fix background resize for mobile

--- src/components/Logistics.js
+++ src/components/Logistics.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import LogisticsMDX from '../../content/logistics.mdx'
-import LogisticsBackground from '../images/LogisticsBackground.png'
+import background from '../images/LogisticsBackground.png'
+import mobileBackground from '../images/logistics_bg_mobile.png'
 import PrizesIcon from '../images/PrizesIcon.png'
 import ScheduleIcon from '../images/ScheduleIcon.png'
 
 
 function Logistics() {
   return (
-    <div key="logistics" className="relative">
-      <img src={LogisticsBackground} className="w-screen" />
-      <a className="anchor" id="logistics"/>
-      <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] bg-linen rounded-lg w-[80%] md:w-[44%] px-14 py-10">
+    <div key="logistics" className="relative mt-16">
+      <img src={background} className="logistics-background hidden md:inline"/>
+      <img src={mobileBackground} className="logistics-background md:hidden"/>
+      <div className="logistics-body">
+        <a className="logistics-anchor" id="logistics"/>
         <LogisticsMDX/>
-        <div className="flex flex-row space-x-4 pt-4">
+        <div className="logistics-icons">
           <IconSquare name={"SCHEDULE"} src={ScheduleIcon}/>
           <IconSquare name={"PRIZES"} src={PrizesIcon}/>
         </div>
@@ -24,8 +26,8 @@ function Logistics() {
 
 function IconSquare({name, src}) {
   return (
-    <div className="flex flex-col items-center justify-center bg-emerald bg-opacity-10 w-[11.5vw] h-[11vw] rounded-lg">
-      <div className="h-[45%] flex items-center"><img src={src}/></div>
+    <div className="logistics-icon-square">
+      <div className="logistics-icon-square-container"><img src={src} className="max-h-[80%]"/></div>
       <div>{name}</div>
     </div>
   )
